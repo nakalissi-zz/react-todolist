058184819676c80e38ce5ff1cd568f7f86f86985
Daniel Calissi Nakatate <nakalissi@gmail.com>
2017-04-11 00:20:52 +1000

    change structure

---
+++ 1-structure/src/js/client.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+//
+// import Archives from './pages/Archives';
+// import Featured from './pages/Featured';
+import Layout from "./pages/Layout";
+// import Settings from './pages/Settings';
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    
+  </Router>,
+app);
