0204d0825e75f356aee631649c0797929b1f3cab
Daniel Calissi Nakatate <nakalissi@gmail.com>
2017-04-11 01:18:18 +1000

    fix project 1

--- 1-structure/src/js/client.js
+++ 1-structure/src/js/client.js
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
-//
-// import Archives from './pages/Archives';
-// import Featured from './pages/Featured';
-import Layout from "./pages/Layout";
-// import Settings from './pages/Settings';
-const app = document.getElementById('app');
 
-ReactDOM.render(
-  <Router history={hashHistory}>
-    
-  </Router>,
-app);
+import Layout from "./components/Layout";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
