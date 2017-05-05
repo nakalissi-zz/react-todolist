ccaff06dfe96e153ca2dee1469da40e8f3f8793a
Daniel Calissi Nakatate <nakalissi@gmail.com>
2017-05-05 11:02:21 +1000

    Redux

---
+++ 4-redux/README.md
@@ -0,0 +1,2 @@
+# react-todolist
+Study of react
---
+++ 4-redux/src/js/client.js
@@ -0,0 +1,38 @@
+import { applyMiddleware, createStore } from 'redux';
+import axios from "axios";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null,
+};
+
+const reducer = (state=initialState, action) => {
+  switch(action.type) {
+    case "FETCH_USERS_PENDING": {
+      return {...state, fetching: true}
+      break;
+    }
+    case "FETCH_USERS_REJECTED": {
+      return {...state, fetching: false}
+      break;
+    }
+    case "FETCH_USERS_FULFILLED": {
+      return {...state, fetching: true, error: action.payload}
+      break;
+    }
+  }
+  return state;
+}
+
+const middleware = applyMiddleware(promise(), thunk, logger())
+const store = createStore(reducer, middleware)
+
+store.dispatch({
+  type: "FETCH_USERS",
+  payload: axios.get("http://rest.learncode.academy/api/wstern/users")
+})
